ec7da1413511547c0ea9973f6fc6c490e47472b5
Richard <rhshadowgreen@gmail.com>
2024-11-01 11:32:27 -0700

    successful request for input file

--- GodotAoc2024/main.gd
+++ GodotAoc2024/main.gd
@@ -1,9 +1,29 @@
 extends Node
 
-
+var day = 1
+var year = 2023
 
 func _ready():
-	pass
+	var http = $HTTPRequest
+	http.request_completed.connect(_on_request_completed)
+	
+	var url = getUrl(year, day)
+	var session = getSession()
+	var headers = ["Content-Type: text/plain", "Cookie: session=" + session]
+	http.request(url, headers, HTTPClient.METHOD_GET)
 
-func _process(delta):
-	pass
+func getSession():
+	var file = FileAccess.open("res://secret.txt", FileAccess.READ)
+	var session = file.get_as_text().strip_edges()
+	return session
+
+func getUrl(y, d):
+	return "https://adventofcode.com/" + str(y) + "/day/" + str(d) +"/input"
+
+func _on_request_completed(result, response_code, headers, body):
+	var dir = DirAccess.open("res://day")
+	if not dir.dir_exists(str(day)):
+		dir.make_dir(str(day))
+	
+	var file = FileAccess.open("res://day/" + str(day) + "/input.txt", FileAccess.WRITE)
+	file.store_string(body.get_string_from_utf8())
